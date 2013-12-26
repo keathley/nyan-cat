6f5e8e762432042ebf4eded0321ff2564e9f377b
Chris Keathley <spyc3r@gmail.com>
2013-12-26 15:46:49 -0500

    Add tests for colors

--- lib/nyan-cat/cat.rb
+++ lib/nyan-cat/cat.rb
@@ -17,9 +17,10 @@ END
 
   class Cat
     def initialize(opts = {})
-      @frames       = opts.fetch(:frames) { [FRAME1, FRAME2] }
-      @trail_length = opts.fetch(:trail_length) { 9 }
-      @frame_count  = 0
+      @frames         = opts.fetch(:frames) { [FRAME1, FRAME2] }
+      @trail_length   = opts.fetch(:trail_length) { 9 }
+      @colorize_trail = opts.fetch(:colorize) { true }
+      @frame_count    = 0
     end
 
     def tick(frame_count = nil)
@@ -45,15 +46,15 @@ END
       frame_with_trails = lines.map.with_index do |line, i|
         @trail_length.times do |j|
           color_index = j - @frame_count % @trail_length
-          line.prepend(Paint[trail_char(index, i, j), rainbow(color_index)])
+          if @colorize_trail
+            line.prepend(Paint[trail_char(index, i, j), rainbow(color_index)])
+          else
+            line.prepend(trail_char(index, i, j))
+          end
         end
         line
       end
       frame_with_trails.join("\n")
-    end
-
-    def trail_char(frame_index, line_index, trail_index)
-      ((frame_index + line_index + trail_index) % 2).zero? ? '-' : '_'
     end
 
     def rainbow(freq = 0.3, i)
@@ -61,6 +62,10 @@ END
       green = Math.sin(freq*i + 2*Math::PI/3) * 127 + 128
       blue  = Math.sin(freq*i + 4*Math::PI/3) * 127 + 128
       "#%02X%02X%02X" % [ red, green, blue ]
+    end
+
+    def trail_char(frame_index, line_index, trail_index)
+      ((frame_index + line_index + trail_index) % 2).zero? ? '-' : '_'
     end
   end
 end
